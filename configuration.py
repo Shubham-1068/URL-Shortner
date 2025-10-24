560615d306cefc52e3ae11943a7f6b67c13ba75b
SHUBHAM <shubhamraj1068@gmail.com>
2025-10-24 23:37:32 +0530

    added dotenv

--- configuration.py
+++ configuration.py
@@ -1,8 +1,11 @@
-
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
-uri = "mongodb+srv://shubhamcsecs:shubhamcsecs@cluster0.ycj0r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+load_dotenv()
+
+uri = os.getenv('MONGODB_URI')
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
